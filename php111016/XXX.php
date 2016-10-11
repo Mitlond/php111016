4abaef00f638f9dcfcdec0ecf9298c31722beaf2
Mitlond <comp122012016@mail.ru>
2016-10-11 17:09:57 +0300

    php

---
+++ php111016/XXX.php
@@ -0,0 +1,11 @@
+<?php
+class XXX{
+	public $A,$D,$ZH;
+	function res($A,$D){
+		$this->A = $A;
+		$this->D = $D;
+		$this->ZH = $A+$D;
+	}
+	
+}
+?>
---
+++ php111016/index.php
@@ -0,0 +1,7 @@
+﻿<?php
+require_once "XXX.php";
+$easy = new XXX;
+$easy->res(2,3);
+echo $easy->ZH;
+
+?>
